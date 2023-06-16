8ed806011457f364b78df51cbf47d37e5923a3db
Batiste Bieler <batiste@nop.ch>
2023-06-16 15:06:01 +0100

    More phone friendly

--- deckhand/main.js
+++ deckhand/main.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0x2c2c2c );
-const w = 420
+const w = Math.min(document.body.clientWidth - 30, 400)
 const h = 490
 const camera = new THREE.PerspectiveCamera( 50, w/h, 1, 2000 );
 
