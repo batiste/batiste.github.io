f2279dfbab180dffac17b03d72f8bd2c3c7685ee
Batiste Bieler <batiste@nop.ch>
2023-08-11 18:40:20 +0100

    Start work on french translations

--- deckhand/cards.fr.js
+++ deckhand/cards.fr.js
@@ -1,31 +1,31 @@
 
 var fr_translations = [
     {
-      "title": "Bagarre à poings",
+      "title": "Bagarre",
       "text": "+1 Trésor.",
       "flavor": "Les amitiés pirates commencent par une bagarre."
     },
     {
       "title": "Célébration",
-      "text": "+1 Trésor pour chaque Célébration que vous avez.<br>+1 Actualisation de la Réserve, vous pouvez piocher une carte.",
+      "text": "+1 Trésor par Célébration que tu as.<br>+1 Recharge, tu peux piocher une carte.",
       "flavor": "Grog solitaire ou rires partagés ?"
     },
     {
       "title": "Taverne",
-      "text": "+1 Carte si vous avez une Aventure.<br>+1 Trésor si vous avez une Attaque.<br>+1 Action si vous avez une Structure.",
-      "flavor": "La taverne rassemble voleurs, coquins et fripouilles."
+      "text": "+1 Carte si tu as une Aventure.<br>+1 Trésor si tu as une Attaque.<br>+1 Action si tu as une Structure.",
+      "flavor": "Elle rassemble voleurs, coquins et fripouilles."
     },
     {
       "title": "Devineuse de Fortune",
-      "text": "Révélez 2 cartes de n'importe quel deck.<br>+1 Carte pour chaque Aventure révélée.<br>+1 Trésor pour chaque Attaque révélée.<br>+1 Action pour chaque Structure révélée."
+      "text": "Révéle 2 cartes de n'importe quel deck.<br>+1 Carte par Aventure révélée.<br>+1 Trésor par Attaque révélée.<br>+1 Action par Structure révélée."
     },
     {
       "title": "Tir rapide",
-      "text": "+1 Trésor.<br>+1 Action si vous avez une Attaque."
+      "text": "+1 Trésor.<br>+1 Action si tu as une Attaque."
     },
     {
       "title": "Maître de la Danse",
-      "text": "+1 Trésor pour chaque carte de coût 1 que vous avez.<br>+1 Action pour chaque carte de coût 3 que vous avez.",
+      "text": "+1 Trésor par carte de coût 1 que tu as.<br>+1 Action par carte de coût 3 que tu as.",
       "flavor": "Les tacticiens accomplis mêlent manœuvres subtiles et gestes grandioses."
     },
     {
@@ -34,52 +34,52 @@ var fr_translations = [
       "flavor": "Parfois, le poison agit lentement."
     },
     {
-      "title": "Limonadier",
-      "text": "+1 Trésor.<br>+1 Trésor si vous êtes l'un des joueurs possédant le moins de cartes.",
-      "flavor": "Les rumeurs disent qu'il est l'homme le plus riche de la ville."
+      "title": "Clochard de Plage",
+      "text": "+1 Trésor. +1 Trésor si tu es l'un des joueurs possédant le moins de cartes.",
+      "flavor": "Les rumeurs disent qu'il est le plus riche du village."
     },
     {
       "title": "Rêverie",
-      "text": "+1 Trésor.<br>Si vous êtes ciblé par des effets d'une carte : Défaussez cette carte pour les annuler."
+      "text": "+1 Trésor.<br>Si tu êtes ciblé par des effets d'une carte : Défausse Rêverie pour les annuler."
     },
     {
       "title": "Leçons de Kung Fu",
-      "text": "+1 Trésor, +1 Actualisation de la Réserve.<br>Mettez 1 carte de la défausse d'un joueur ciblé en bas de son deck.",
-      "flavor": "Les marées, utilisez leur pouvoir à votre avantage !"
+      "text": "+1 Trésor, +1 Recharge.<br>Mets 1 carte de la défausse d'un joueur ciblé en bas de son deck.",
+      "flavor": "Les marées, utilises leur pouvoir à ta avantage !"
     },
     {
       "title": "Mariage du Gouverneur",
-      "text": "+1 Trésor. Au prochain tour de l'ennemi ciblé, il gagne +1 Achat et doit vous donner un Trésor avant son premier achat.",
+      "text": "+1 Trésor. Au prochain tour de l'ennemi ciblé, il gagne +1 Achat et doit tu donner un Trésor avant son premier achat.",
       "flavor": "Un somptueux cadeau assure des faveurs et de l'influence."
     },
  {
     "title": "L'alchimie",
-    "text": "Défaussez un nombre quelconque de cartes, obtenez autant de Trésors. +2 Cartes.",
+    "text": "Défausse un nombre quelconque de cartes, obtient autant de Trésors. +2 Cartes.",
     "flavor": "Infusée de feu, tordant le destin. Qu'est-ce qui pourrait mal tourner ?"
   },
   {
     "title": "Caprice de l'Empereur",
-    "text": "+1 Actualisation de la Réserve. Un ennemi choisi sélectionne 3 cartes de la réserve. Vous pouvez appliquer les effets de l'une d'entre elles comme si vous veniez de la jouer.",
+    "text": "+1 Recharge. Un ennemi sélectionne 3 cartes de la réserve. Tu peux appliquer les effets de l'une d'entre elles comme si tu venais de la jouer.",
     "flavor": "Dans ses mains royales, même vos choix sont illusoires."
   },
   {
     "title": "Maîtrise des Cartes",
-    "text": "+1 Action.<br>Mettez une carte de votre défausse dans votre main.<br>Quand défaussée : +1 Carte.",
+    "text": "+1 Action.<br>Mets une carte de ta défausse dans ta main.<br>Quand défaussée : +1 Carte.",
     "flavor": "Un mouvement de poignet commande l'émerveillement et la peur."
   },
   {
     "title": "Commerce d'Épices",
-    "text": "+1 Trésor.<br>Si cette carte est dans votre défausse, la première Structure que vous achetez chaque tour coûte 1 de moins.",
+    "text": "+1 Trésor.<br>Si cette carte est dans ta défausse, la première Structure que tu achètes chaque tour coûte 1 de moins.",
     "flavor": "Les épices dansent et se mêlent, un goût d'opulence."
   },
   {
     "title": "Planque",
-    "text": "+2 Cartes, +1 Trésor.<br>Quand défaussée : vous pouvez la remettre dans votre main. Ne le faites qu'une fois par pioche.",
+    "text": "+2 Cartes, +1 Trésor.<br>Quand défaussée : tu peux la remettre dans ta main. Ne le fait qu'une fois par pioche.",
     "flavor": "Un refuge pour ouvrir une bouteille de rhum et la finir."
   },
   {
     "title": "Mine d'Or",
-    "text": "Un ennemi choisi choisit un nombre. +3 Cartes. Si le coût total des cartes piochées n'est pas ce nombre : révélez-les, +1 Action.",
+    "text": "Un ennemi choisi choisit un nombre. +3 Cartes. Si le coût total des cartes piochées n'est pas ce nombre : révéle-les, +1 Action.",
     "flavor": "Sous le sol, se cachent rêves et désespoir."
   },
   {
@@ -89,24 +89,24 @@ var fr_translations = [
   },
   {
     "title": "Bibliothèque Antique",
-    "text": "+2 Trésors.<br>Quand défaussée : vous pouvez mélanger votre défausse dans votre deck.",
+    "text": "+2 Trésors.<br>Quand défaussée : tu peux mélanger ta défausse dans ta deck.",
     "flavor": "Chaque page dévoile un secret impérial."
   },
   {
     "title": "Usine de Rhum",
-    "text": "+2 Trésors. Vous pouvez donner un Trésor de la réserve à tout le monde. Si vous le faites, tout le monde doit défausser une carte après leur pioche initiale lors de leur prochain tour."
+    "text": "+2 Trésors. Tu peux donner un Trésor de la réserve à tout le monde. Si tu le fait, tout le monde doit défausser une carte après leur pioche initiale lors de leur prochain tour."
   },
   {
     "title": "Exploration",
-    "text": "+1 Action si vous avez une Aventure.<br>+1 Carte, +1 Trésor.",
+    "text": "+1 Action si tu as une Aventure.<br>+1 Carte, +1 Trésor.",
     "flavor": "Mettez les voiles, mais pas sans une boussole en main."
   },
   {
     "title": "Carte au Trésor",
-    "text": "+1 Trésor. Regardez les 3 cartes du dessus de votre deck. Vous pouvez en mettre autant que vous voulez dans votre défausse et le reste sur le dessus de votre deck dans n'importe quel ordre. +1 Carte."
+    "text": "+1 Trésor. Regarde les 3 cartes du dessus de ton deck. Tu peux en mettre autant que tu veux dans ta défausse et le reste sur le dessus de ton deck dans n'importe quel ordre. +1 Carte."
   },
   {
     "title": "Singe Malicieux",
-    "text": "+1 Action, +1 Actualisation de la Réserve. Si joué depuis votre main, échangez Singe Malicieux ou une carte de votre main avec une carte de coût égal de la réserve. La nouvelle carte rejoint votre main."
+    "text": "+1 Action, +1 Recharge. Si joué depuis ta main, échange Singe Malicieux ou une carte de ta main avec une carte de coût égal de la réserve. La nouvelle carte rejoint ta main."
   }
 ]
