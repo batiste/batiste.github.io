b00c89a4226c4310b6dd85e627da309ff70a766c
Batiste Bieler <batiste@nop.ch>
2023-08-15 10:05:42 +0100

    Tweak to text and marketing images

--- deckhand/cards.fr.js
+++ deckhand/cards.fr.js
@@ -16,7 +16,7 @@ var fr_translations = [
   },
   {
     "title":"Voyante",
-    "text":"Révéle 2 cartes de n'importe quel deck.<br>+1 Carte par Aventure révélée.<br>+1 Trésor par Attaque révélée.<br>+1 Action par Structure révélée."
+    "text":"Révéle 2 cartes de n’importe quel deck.<br>+1 Carte par Aventure révélée.<br>+1 Trésor par Attaque révélée.<br>+1 Action par Structure révélée."
   },
   {
     "title":"Tir rapide",
@@ -24,7 +24,7 @@ var fr_translations = [
     "flavor-no-quotes": "« Ne les laissez pas se regrouper ! » ordonne-t-elle, les pistolets crépitant."
   },
   {
-    "title": "Maître d'Arme",
+    "title": "Maître d’Arme",
     "text":"+1 Trésor par carte de coût 1 en main.<br>+1 Action par carte de coût 3 en main.",
     "flavor":"Les tacticiens accomplis mêlent manœuvres subtiles et gestes grandioses."
   },
@@ -35,38 +35,38 @@ var fr_translations = [
   },
   {
     "title":"Flâneur de Plage",
-    "text":"+1 Trésor. +1 Trésor si tu es l'un des joueurs possédant le moins de cartes.",
+    "text":"+1 Trésor. +1 Trésor si tu es l’un des joueurs possédant le moins de cartes.",
     "flavor":"Les rumeurs courent qu'il est le plus riche du village."
   },
   {
-    'title': 'Paresse Karmique',
-    'text': 'Choisis une carte de ta main ou défausse. L\'ennemi sélectionné l\'échange avec une de la réserve coûtant 1 de plus.',
-    'flavor-no-quotes': '« Harmonie ! Sérénité ! » bâilla-t-il, après sa sieste.'
+    "title": "Paresse Karmique",
+    "text": "Choisis une carte de ta main ou défausse. Un ennemi choisi l’échange avec une de la réserve coûtant 1 de plus.",
+    "flavor-no-quotes": "« Harmonie ! Sérénité ! » bâilla-t-il, après sa sieste."
   },
   // {
   //   "title":"Rêverie",
-  //   "text":"+1 Trésor.<br>Si tu es ciblé par des effets d'une carte : Défausse Rêverie pour les annuler.",
+  //   "text":"+1 Trésor.<br>Si tu es ciblé par des effets d’une carte : Défausse Rêverie pour les annuler.",
   //   "flavor-no-quotes": "\"Harmonie ! Sérénité !\" bâilla-t-il après sa sieste.",
   // },
   {
     "title":"Leçons de Kung Fu",
-    "text":"+1 Trésor, +1 Card, +1 Recharge.<br>Mets 1 carte de la défausse d'un joueur ciblé en dessous de son deck.",
+    "text":"+1 Trésor, +1 Card, +1 Recharge.<br>Mets 1 carte de la défausse d’un joueur ciblé en dessous de son deck.",
     "flavor":"Les marées, utilises leur pouvoir à ton avantage !"
   },
   {
     "title":"Mariage du Gouverneur",
-    "text":"+1 Trésor. Au prochain tour de l'ennemi ciblé, il gagne +1 Achat et doit tu donner un Trésor avant son premier achat.",
+    "text":"+1 Trésor. Au prochain tour de l’ennemi ciblé, il gagne +1 Achat et doit tu donner un Trésor avant son premier achat.",
     "flavor":"Un luxueux cadeau assure faveurs et influence."
   },
   {
     "title":"Poudre Impériale",
-    "text":"+1 Trésor. Si tu perds possession de cette carte : retire du jeu une carte de la défausse de l'ennemi ciblé coûtant 3 ou moins.",
+    "text":"+1 Trésor. Si tu perds possession de cette carte : retire du jeu une carte de la défausse de l’ennemi ciblé coûtant 3 ou moins.",
     "flavor":"ATTENTION : ne pas rouler, choquer ou vaciller."
   },
   {
     "title":"Marchandage",
     "text":"+1 Recharge, +1 Achat.<br>Ton prochain achat ce tour-ci coûte 1 de moins.",
-    "flavor":"Parfois, la langue est plus tranchante que l'épée."
+    "flavor":"Parfois, la langue est plus tranchante que l’épée."
   },
   {
     "title":"Voleur de Ruelle",
@@ -76,12 +76,12 @@ var fr_translations = [
   {
     "title":"Totem Inutile",
     "text":"Le Totem Inutile est à la fois une Structure, une Attaque et une Aventure.",
-    "flavor":"Le bibelot futile semble ricaner d'un air moqueur."
+    "flavor":"Le bibelot futile semble ricaner d’un air moqueur."
   },
   {
     "title":"Hôtel du Conseil",
     "text":"Un ennemi choisit une carte de la réserve et la place dans ta défausse.",
-    "flavor":"De l'or pour un siège, une richesse gaspillée en chuchotements."
+    "flavor":"De l’or pour un siège, une richesse gaspillée en chuchotements."
   },
   {
     "title": "Typhon",
@@ -95,8 +95,8 @@ var fr_translations = [
     "flavor":"Imprégné de feu, tordant le destin. Qu'est-ce qui pourrait mal tourner ?"
   },
   {
-    "title":"Caprice de l'Empereur",
-    "text":"+1 Recharge. Un ennemi choisi 3 cartes de la réserve. Tu peux appliquer les effets de l'une d'entre elles.",
+    "title":"Caprice de l’Empereur",
+    "text":"+1 Recharge. Un ennemi choisi 3 cartes de la réserve. Tu peux appliquer les effets de l’une d’entre elles.",
     "flavor":"Dans ses mains royales vos choix sont illusoires."
   },
   {
@@ -105,9 +105,9 @@ var fr_translations = [
     "flavor":"Un geste du poignet provoque surprise et frayeur."
   },
   {
-    "title":"Négoce d'Épices",
+    "title":"Négoce d’Épices",
     "text":"+1 Trésor. Si cette carte est dans ta défausse, la première Structure que tu achètes chaque tour coûte 1 de moins.",
-    "flavor":"Épices dansantes, saveurs d'opulence."
+    "flavor":"Épices dansantes, saveurs d’opulence."
   },
   {
     "title":"Refuge",
@@ -115,8 +115,8 @@ var fr_translations = [
     "flavor":"Un refuge pour ouvrir un flacon de rhum et le finir."
   },
   {
-    "title":"Mine d'Or",
-    "text":"Un ennemi choisi un nombre. +3 Cartes. Si le coût total des cartes piochées n'est pas ce nombre : révèle-les, +1 Action.",
+    "title":"Mine d’Or",
+    "text":"Un ennemi choisi un nombre. +3 Cartes. Si le coût total des cartes piochées n’est pas ce nombre : révèle-les, +1 Action.",
     "flavor":"Sous le sol, se cachent rêves et désespoir."
   },
   {
@@ -141,7 +141,7 @@ var fr_translations = [
   {
     "title":"Carte au Trésor",
     "text":`+1 Trésor. Regarde les 3 cartes du dessus de ton deck. Défausse-en autant que tu veux 
-    et mets le reste sur le dessus de ton deck dans n'importe quel ordre. +1 Carte.`
+    et mets le reste sur le dessus de ton deck dans n’importe quel ordre. +1 Carte.`
   },
   {
     "title":"Rage Simiesque",
@@ -159,8 +159,8 @@ var fr_translations = [
   },
   {
     "title": "Vagues Inflexibles",
-    "text": "+1 Action. +1 Recharge. Déplace jusqu'à 3 cartes de la défausse de n'importe quel nombre de joueurs ciblés en dessous de leur deck.",
-    // "flavor": "Les forces de l'océan sont indomptables."
+    "text": "+1 Action. +1 Recharge. Déplace jusqu'à 3 cartes de la défausse de n’importe quel nombre de joueurs ciblés en dessous de leur deck.",
+    // "flavor": "Les forces de l’océan sont indomptables."
   },
   {
     "title": "Expédition Perdue",
@@ -169,7 +169,7 @@ var fr_translations = [
   },
   {
     "title": "Observateur Furtif",
-    "text": "Révèle les 2 premières cartes du deck de l'ennemi ciblé. Place-en une dans sa défausse et gagne des Trésors équivalant aux points de victoire de la carte.",
+    "text": "Révèle les 2 premières cartes du deck de l’ennemi ciblé. Place-en une dans sa défausse et gagne des Trésors équivalant aux points de victoire de la carte.",
     // "flavor": "Un œil vigilant révèle les vérités cachées."
     "flavor": ""
   },
@@ -190,7 +190,7 @@ var fr_translations = [
   {
     "title": "Affaire de Singe",
     "text": "Retire 1 carte de ta main ou défausse du jeu&nbsp;: Gagne des Trésors et des Actions équivalant aux points de victoire de la carte.",
-    "flavor": "Les espiègleries de l'un sont l'aubaine d'un autre."
+    "flavor": "Les espiègleries de l’un sont l’aubaine d’un autre."
   },
   {
     "title": "Vent dans les Voiles",
@@ -202,7 +202,7 @@ var fr_translations = [
     "flavor": "Là où quêtes et bravoure convergent."
   },
   {
-    "title": "Pied de l'Arc-en-ciel",
+    "title": "Pied de l’Arc-en-ciel",
     "text": "+1 Carte.<br>Au début de ton tour, si cette carte est dans ta défausse, ainsi qu'une Attaque et une Structure : +1 Action."
   },
   {
@@ -216,7 +216,7 @@ var fr_translations = [
     "flavor": "Au bord de la mer, la citadelle appelle les intrépides à la recherche de fortunes."
   },
   {
-    "title": "Atelier d'Architectes",
+    "title": "Atelier d’Architectes",
     "text": "+1 Action, +1 Trésor. S'il y a 2 structures ou plus dans la réserve : +1 Achat.",
     "flavor": "Des plans méticuleux et un artisanat habile donnent vie aux grandes visions."
   },
@@ -227,7 +227,7 @@ var fr_translations = [
   },
   {
     "title": "Maraudeur des Abysses",
-    "text": "+1 Action. Échange une carte de ta main avec une carte du même coût de la défausse d'un ennemi.",
+    "text": "+1 Action. Échange une carte de ta main avec une carte du même coût de la défausse d’un ennemi.",
     "flavor": "Né des profondeurs, il se nourrit des incertitudes."
   },
   {
@@ -242,7 +242,7 @@ var fr_translations = [
   },
   {
     "title": "Course Désespérée",
-    "text": "Toi et un ennemi révélez les 3 premières cartes de vos decks. Si tu as révélé plus d'Attaques : mets une carte de la réserve coûtant 4 ou moins dans ta défausse."
+    "text": "Toi et un ennemi révélez les 3 premières cartes de vos decks. Si tu as révélé plus d’Attaques : mets une carte de la réserve coûtant 4 ou moins dans ta défausse."
   },
   {
     "title": "Grenade Improvisée",
@@ -254,12 +254,12 @@ var fr_translations = [
   },
   {
     "title": "Poupée Envoûtée",
-    "text": "Lors du prochain tour de l'ennemi ciblé, joue sa première action. Prends toutes les décisions découlant de cette action.",
+    "text": "Lors du prochain tour de l’ennemi ciblé, joue sa première action. Prends toutes les décisions découlant de cette action.",
     "flavor": "Le destin est un concept fragile."
   },
   {
     "title": "Marchand Sournois",
-    "text": "Échange une carte entre ta défausse et la défausse de l'ennemi ciblé tant que la différence de coût est de 1 ou moins. Donne un Trésor de la banque à l'ennemi."
+    "text": "Échange une carte entre ta défausse et la défausse de l’ennemi ciblé tant que la différence de coût est de 1 ou moins. Donne un Trésor de la banque à l’ennemi."
   },
   {
     "title": "Mercenaire des Néréides",
@@ -268,8 +268,8 @@ var fr_translations = [
   },
   {
     "title": "Fureur du Kraken",
-    "text": "Tous les joueurs défaussent leur main. Applique les effets d'une carte de n'importe quelle défausse.",
-    "flavor": "Méfiez-vous du dévoreur de navires et d'âmes."
+    "text": "Tous les joueurs défaussent leur main. Applique les effets d’une carte de n’importe quelle défausse.",
+    "flavor": "Méfiez-vous du dévoreur de navires et d’âmes."
   },
   {
     "title": "Île du Crâne",
@@ -278,10 +278,10 @@ var fr_translations = [
   {
     "title": "Chasse au Trésor",
     "text": "+1 Action, +1 Achat,<br>+1 Carte, +1 Trésor.",
-    "flavor": "Une île enveloppée de mystère détient la clé d'une immense richesse."
+    "flavor": "Une île enveloppée de mystère détient la clé d’une immense richesse."
   },
   {
     "title": "Le Grand Conseil",
-    "text": "+2 Actions. Avant la fin de ton tour, retourne 4 cartes de la réserve face cachée. Ces cartes n'existent pas. Restaure-les à tout moment lors de ton prochain tour."
+    "text": "+2 Actions. Avant la fin de ton tour, retourne 4 cartes de la réserve face cachée. Ces cartes n’existent pas. Restaure-les à tout moment lors de ton prochain tour."
   }
 ]
