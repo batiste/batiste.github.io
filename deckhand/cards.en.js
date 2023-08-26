ca97b33e5943efe1488c75ec865193ad87c910c4
Batiste Bieler <batiste@nop.ch>
2023-08-26 12:25:13 +0100

    Use coins

--- deckhand/cards.en.js
+++ deckhand/cards.en.js
@@ -2,7 +2,7 @@
 var cards = [
 {
     'title': 'Fist Fight',
-    'text': '+1 :Treasure:<br><i>(Take a Treasure from the bank.)</i>',
+    'text': '+1 :Coin:<br><i>(Take a Coin from the bank.)</i>',
     'flavor': 'Pirate friendships start with a fight.',
     'img': 'attacks/fist-fight.png',
     'type': 'attack',
@@ -12,7 +12,7 @@ var cards = [
 },
 {
     'title': 'Celebrate',
-    'text': '+1 :Card: <i>(You may draw a card.)</i><br>+1 :Reload: <i>(You may reload a supply card.)</i><br>Then +1 :Treasure: for each Celebrate in hand.',
+    'text': '+1 :Card: <i>(You may Draw a card.)</i><br>+1 :Reload: <i>(You may Reload a supply card.)</i><br>Then +1 :Coin: for each Celebrate in hand.',
     'flavor': 'Lonely grog or shared laughter?',
     'img': 'adventures/celebrate.png',
     'type': 'adventure',
@@ -23,7 +23,7 @@ var cards = [
 {
     'title': 'Tavern',
     'text': `If you have an Adventure in hand: +1 :Card:.<br>
-        If you have an Attack in hand: +1 :Treasure:.<br>
+        If you have an Attack in hand: +1 :Coin:.<br>
         If you have a Structure in hand: +1 :Action:.`,
     'flavor': 'The tavern unites thieves, rogues and scoundrels.',
     'img': 'adventures/tavern-3.png',
@@ -37,7 +37,7 @@ var cards = [
     'title': 'Fortune Teller',
     'text': `Reveal the 2 top cards from any one deck.<br>
         +1 :Card: for each Adventure revealed.<br>
-        +1 :Treasure: for each Attack revealed.<br>
+        +1 :Coin: for each Attack revealed.<br>
         +1 :Action: for each Structure revealed.`,
     //'flavor': 'The tavern unites thieves, rogues and scoundrels.',
     'img': 'adventures/fortune-teller.png',
@@ -49,7 +49,7 @@ var cards = [
 
 {
     'title': 'Quick Shot',
-    'text': '+1 :Treasure:.<br>If you have an Attack in hand: +1 :Action:.',
+    'text': '+1 :Coin:.<br>If you have an Attack in hand: +1 :Action:.',
     'flavor-no-quotes': '“Don’t let them regroup!” she commands, with pistols ablaze.',
     'img': 'attacks/dual-pistol.png',
     'type': 'attack',
@@ -60,7 +60,7 @@ var cards = [
 
 {
     'title': 'Blademaster',
-    'text': '+1 :Treasure: for each 1 cost card in hand.<br>+1 :Action: for each 3 cost card in hand.',
+    'text': '+1 :Coin: for each 1 cost card in hand.<br>+1 :Action: for each 3 cost card in hand.',
     'flavor': `Accomplished tacticians blend subtle manoeuver with grand gestures.`,
     'img': 'attacks/dance-master-3.png',
     'type': 'attack',
@@ -71,7 +71,7 @@ var cards = [
 
 {
     'title': 'Poisoned Grog',
-    'text': '+1 :Treasure:, +1 :Reload:.<br>When Discarded: +1 :Treasure:.', // , add condition, if you have not treasure if too strong 
+    'text': '+1 :Coin:, +1 :Reload:.<br>When Discarded: +1 :Coin:.', // , add condition, if you have not treasure if too strong 
     'flavor': 'Sometimes, poison is slow-acting.',
     'img': 'attacks/pint.png',
     'type': 'attack',
@@ -84,7 +84,7 @@ var cards = [
 
 {
     'title': 'Beach Bum',
-    'text': '+1 :Treasure:.<br>If you are one of the players that owns the least amount of cards +1 :Treasure:.',
+    'text': '+1 :Coin:.<br>If you are one of the players that owns the least amount of cards +1 :Coin:.',
     'flavor': 'Rumors have it that he is the richest man in town.',
     'img': 'adventures/beach-bum.png',
     'type': 'adventure',
@@ -94,21 +94,9 @@ var cards = [
 
 },
 
-// {
-//     'title': 'Reverie',
-//     'text': '+1 :Treasure:.<br>If you are targeted by any effects from one card: Discard this card to cancel them.',
-//     'flavor-no-quotes': '“Harmony! Serenity!” he yawned, after his nap.',
-//     // 'reduced-font-size': '9.1px',
-//     'img': 'adventures/namaste.png',
-//     'type': 'adventure',
-//     'victory': '1',
-//     'cost': '1',
-//     'amount': 1,
-// },
-
 {
     'title': 'Karmic Elevation',
-    'text': 'Pay 1 :Treasure:: Reveal a card from your hand. A chosen enemy pick a supply card that costs 1 more. Swap those 2 cards.',
+    'text': 'Pay 1 :Coin:: Reveal a card from your hand. A chosen enemy pick a supply card that costs 1 more. Swap those 2 cards.',
     'flavor-no-quotes': '“Harmony! Serenity!” he yawned, after his nap.',
     // 'reduced-font-size': '9.1px',
     'img': 'adventures/namaste.png',
@@ -120,7 +108,7 @@ var cards = [
 
 {
     'title': 'Kung Fu Lessons',
-    'text': '+1 :Treasure:, +1 :Card:, +1 :Reload:.<br>Put 1 card from target player’s stash at the bottom of their deck.',
+    'text': '+1 :Coin:, +1 :Card:, +1 :Reload:.<br>Put 1 card from target player’s stash at the bottom of their deck.',
     'flavor': 'The tide, harness its power to your advantage!',
     // 'reduced-font-size': '9.1px',
     'img': 'adventures/kung-fu.png',
@@ -134,7 +122,7 @@ var cards = [
 
 {
     'title': 'Governor’s Wedding',
-    'text': '+1 :Treasure:. On their next turn, target enemy gains +1 :Buy: and must give you 1 Treasure before their first Buy.',
+    'text': '+1 :Coin:. On their next turn, target enemy gains +1 :Buy: and must give you 1 Coin before their first Buy.',
     'flavor': 'A sumptuous gift secure favors and influence.',
     'img': 'structures/wedding.png',
     'type': 'attack',
@@ -146,7 +134,7 @@ var cards = [
 
 {
     'title': 'Imperial Powder',
-    'text': '+1 :Treasure:. If you lose ownership of this card: Remove from the game a card from target enemy’s stash that costs 2 or less.',
+    'text': '+1 :Coin:. If you lose ownership of this card: Remove from the game a card from target enemy’s stash that costs 2 or less.',
     'flavor': 'WARNING: does not roll, shock, or sway.',
     'img': 'structures/powder-keg.png',
     'type': 'structure',
@@ -169,7 +157,7 @@ var cards = [
 
 {
     'title': 'Alley Thief',
-    'text': '+1 :Treasure:, +1 :Reload:.:or:Steal a Treasure from target player.',
+    'text': '+1 :Coin:, +1 :Reload:.:or:Steal a Coin from target player.',
     'flavor': 'A fleeting shadow, your treasures vanish!',
     'img': 'attacks/thief.png',
     'type': 'attack',
@@ -205,7 +193,7 @@ var cards = [
 
 {
     'title': 'Typhoon',
-    'text': `+2 :Card:, +1 :Discard:, +1 :Treasure:. Then all players simultaneously
+    'text': `+2 :Card:, +1 :Discard:, +1 :Coin:. Then all players simultaneously
      move a card from their stash or the top card of their deck (no peeking) to their left neighbor’s stash.`,
     //'flavor': 'A Swirling chaos! A navigational nightmare!',
     'reduced-font-size': '9.3px',
@@ -218,7 +206,7 @@ var cards = [
 
 {
     'title': 'Alchemy',
-    'text': 'Discard any number of cards to get that many Treasures. Then +2 :Card:.',
+    'text': 'Discard any number of cards to get that many Coins. Then +2 :Card:.',
     'flavor': 'Imbued with fire, twisting fate. What could possibly go awry?',
     'img': 'adventures/alchemist-2.png',
     'type': 'adventure',
@@ -253,7 +241,7 @@ var cards = [
 
 {
     'title': 'Spice Trade',
-    'text': '+1 :Treasure:.<br>If this card is in your stash, the first Structure you Buy each turn costs 1 less.',
+    'text': '+1 :Coin:.<br>If this card is in your stash, the first Structure you Buy each turn costs 1 less.',
     'flavor': 'Spices dance and blend, a taste of opulence.',
     'img': 'adventures/spice-trade.png',
     'type': 'adventure',
@@ -265,7 +253,7 @@ var cards = [
 
 {
     'title': 'Hideout',
-    'text': '+2 :Card:, +1 :Treasure:.<br>When Discarded: +1 :Card:',
+    'text': '+2 :Card:, +1 :Coin:.<br>When Discarded: +1 :Card:',
     'flavor': 'A haven to open a rum bottle and see it through.',
     'img': 'structures/hideout.png',
     'type': 'structure',
@@ -291,7 +279,7 @@ var cards = [
 
 {
     'title': 'Harbor Market',
-    'text': '+2 :Treasure:.:or:+1 :Treasure:, +1 :Buy:.',
+    'text': '+2 :Coin:.:or:+1 :Coin:, +1 :Buy:.',
     'flavor': 'Secrets and stolen treasures traded here.',
     'img': 'structures/market-super-format.png',
     'type': 'structure',
@@ -302,7 +290,7 @@ var cards = [
 
 {
     'title': 'Ancient Library',
-    'text': '+2 :Treasure:.<br>When Discarded: You may shuffle your stash into your deck.',
+    'text': '+2 :Coin:.<br>When Discarded: You may shuffle your stash into your deck.',
     'flavor': 'Each page unravels an imperial secret.',
     'img': 'structures/archives.png',
     'type': 'structure',
@@ -314,7 +302,7 @@ var cards = [
 
 {
     'title': 'Rum Factory',
-    'text': '+2 :Treasure:. You may give a Treasure from the bank to every player. If you do, each player must Discard a card after their initial draw on their next turn.',
+    'text': '+2 :Coin:. You may give a Coin from the bank to every player. If you do, each player must Discard a card after their initial draw on their next turn.',
     'img': 'structures/rum3.png',
     'type': 'structure',
     'cost': '2',
@@ -324,7 +312,7 @@ var cards = [
 
 {
     'title': 'Exploration',
-    'text': 'If you have an Adventure in<br>hand: +1 :Action:.<br>Then +1 :Card:, +1 :Treasure:.',
+    'text': 'If you have an Adventure in<br>hand: +1 :Action:.<br>Then +1 :Card:, +1 :Coin:.',
     'flavor': 'Set sail, but not without a compass in hand.',
     'img': 'adventures/exploration.png',
     'type': 'adventure',
@@ -335,7 +323,7 @@ var cards = [
 
 {
     'title': 'Treasure Map',
-    'text': '+1 :Treasure:. Look at the top 3 cards of your deck. You may put any number of them into your stash and the rest on top of your deck in any order. +1 :Card:.',
+    'text': '+1 :Coin:. Look at the top 3 cards of your deck. You may put any number of them into your stash and the rest on top of your deck in any order. +1 :Card:.',
     //'flavor': 'Riches await the keen-eyed adventurer..',
     'img': 'adventures/treasure-map.png',
     'type': 'adventure',
@@ -356,7 +344,7 @@ var cards = [
 
 {
     'title': 'Ghost Crew',
-    'text': '+1 :Card:, +1 :Treasure:. You may put this card on the top of its owner’s deck. If there is 3 or more Attacks in the supply: +1 :Action:',
+    'text': '+1 :Card:, +1 :Coin:. You may put this card on the top of its owner’s deck. If there is 3 or more Attacks in the supply: +1 :Action:',
     'img': 'attacks/ghost-crew.png',
     'flavor': 'Trade your mortal life, enjoy eternal retribution.',
     'type': 'attack',
@@ -367,7 +355,7 @@ var cards = [
 
 {
     'title': 'Haunted Cave',
-    'text': '+2 :Card:. Then +1 :Treasure: for each Attack in hand, up to a maximum of 3 Treasures.',
+    'text': '+2 :Card:. Then +1 :Coin: for each Attack in hand, up to a maximum of 3 Coins.',
     'flavor': 'This place resonates with skeletal clatters. Fear not, for the pure-hearted and free of greed.',
     'img': 'attacks/infested-cave.png',
     'type': 'attack',
@@ -378,7 +366,7 @@ var cards = [
 
 // {
 //     'title': 'Unyielding Waves',
-//     'text': '+1 :Treasure: for each different type of card into your stash (Structure, Adventure, Attack).',
+//     'text': '+1 :Coin: for each different type of card into your stash (Structure, Adventure, Attack).',
 //     'flavor': 'The forces of the roaring ocean cannot be fought.',
 //     'img': 'attacks/relentless-waves.png',
 //     'type': 'attack',
@@ -390,7 +378,7 @@ var cards = [
 
 {
     'title': 'Governor’s Jail',
-    'text': `+2 :Treasure:. Flip 1 supply card face down.
+    'text': `+2 :Coin:. Flip 1 supply card face down.
     This card does not exist. Restore it at any time on your next turn.`,
     'flavor': 'Dear guest, you’re truly at your best restrained.',
     'img': 'structures/jail.png',
@@ -402,7 +390,7 @@ var cards = [
 
 {
     'title': 'Lost Expedition',
-    'text': 'Remove a card from target player’s stash and place 2 bank Treasures on it. When their stash is shuffled, take the Treasures and return the card to their new stash.',
+    'text': 'Remove a card from target player’s stash and place 2 bank Coins on it. When their stash is shuffled, take the Coins and return the card to their new stash.',
     'img': 'adventures/lost-expedition-2.png',
     //'reduced-font-size': '9.0px',
     'type': 'adventure',
@@ -415,7 +403,7 @@ var cards = [
 {
     'title': 'Stealthy Observer',
     'text': `Reveal the top 2 cards of target enemy’s deck. Put 1 revealed card in their stash and gain 
-        Treasures equal to its victory points.`,
+        Coins equal to its victory points.`,
     'flavor': 'A vigilant eye  brings forth hidden truths.',
     'img': 'adventures/roof-thief.png',
     'type': 'adventure',
@@ -427,7 +415,7 @@ var cards = [
 
 {
     'title': 'Extravagance',
-    'text': '+1 :Reload:, +2 :Buy:.:or:+1 :Treasure:, +1 :Buy:.',
+    'text': '+1 :Reload:, +2 :Buy:.:or:+1 :Coin:, +1 :Buy:.',
     'flavor': 'Only one way to spend: lavishly and flamboyantly!',
     'img': 'adventures/extravagance-2.png',
     'type': 'adventure',
@@ -438,7 +426,7 @@ var cards = [
 
 {
     'title': 'Shipyard',
-    'text': '+1 :Treasure:.<br>+1 :Action: for each Structure in your stash. <i>(Do not use more than 5 Actions per turn.)</i>',
+    'text': '+1 :Coin:.<br>+1 :Action: for each Structure in your stash. <i>(Do not use more than 5 Actions per turn.)</i>',
     'img': 'structures/shipyard.png',
     'flavor': 'Every plank aspires to endless horizons.',
     'type': 'structure',
@@ -450,7 +438,7 @@ var cards = [
 
 {
     'title': 'Walk the Plank',
-    'text': ' +2 :Treasure:. You may remove a card from your hand or stash from the game. Gain as many Treasures as the cost of the removed card.',
+    'text': ' +2 :Coin:. You may remove a card from your hand or stash from the game. Gain as many Coins as the cost of the removed card.',
     'flavor': 'A dance on the plank or a delayed sentence?',
     'img': 'attacks/plank-2.png',
     'type': 'attack',
@@ -461,7 +449,7 @@ var cards = [
 
 {
     'title': 'Monkey’s Bargain',
-    'text': 'Remove a card from your hand or stash from the game. For each victory points of the removed card: +1 :Treasure: and +1 :Action:',
+    'text': 'Remove a card from your hand or stash from the game. For each victory points of the removed card: +1 :Coin: and +1 :Action:',
     'flavor': 'A monkey’s mischief is a pirate’s boon.',
     'img': 'attacks/exchange.png',
     'type': 'attack',
@@ -483,7 +471,7 @@ var cards = [
 
 {
     'title': 'Adventurer’s Guild',
-    'text': '+1 :Treasure: for each Adventure in the supply, up to a maximum of 3 Treasures.',
+    'text': '+1 :Coin: for each Adventure in the supply, up to a maximum of 3 Coins.',
     'flavor': 'Where quests and bravery converge.',
     'img': 'structures/adventurer-center.png',
     'type': 'structure',
@@ -508,7 +496,7 @@ var cards = [
 
 {
     'title': 'Bank Fortress',
-    'text': '+2 :Treasure:.<br>When Discarded: +1 :Buy:.',
+    'text': '+2 :Coin:.<br>When Discarded: +1 :Buy:.',
     'flavor': 'Fortified against raiders, the bank safeguards against the mightiest of temptations.',
     'img': 'structures/bank-2.png',
     'type': 'structure',
@@ -532,7 +520,7 @@ var cards = [
 
 {
     'title': 'Architect’s Workshop',
-    'text': '+1 :Treasure:, +1 :Action:. If there is 2 or more Structures in the supply: +1 :Buy:.',
+    'text': '+1 :Coin:, +1 :Action:. If there is 2 or more Structures in the supply: +1 :Buy:.',
     'flavor': 'Meticulous plans and skilled craftsmanship bring grand visions to life',
     'img': 'structures/architect-workshop.png',
     'type': 'structure',
@@ -567,7 +555,7 @@ var cards = [
 
 {
     'title': 'Smuggler’s Cache',
-    'text': '+1 :Treasure:.<br>At the start of your turn, if this card is in your stash: +1 :Treasure:.',
+    'text': '+1 :Coin:.<br>At the start of your turn, if this card is in your stash: +1 :Coin:.',
     'flavor': 'Few can chart the course to the forbidden spoils.',
     'img': 'structures/smugglers-cache.png',
     'type': 'structure',
@@ -579,7 +567,7 @@ var cards = [
 
 {
     'title': 'Gambling Addiction',
-    'text': '+2 :Reload:. If the supply has 4 or more cards of the same type: +3 :Treasure:.',
+    'text': '+2 :Reload:. If the supply has 4 or more cards of the same type: +3 :Coin:.',
     'flavor': 'The allure of easy riches sealed his downfall.',
     'img': 'adventures/gambling.png',
     'type': 'adventure',
@@ -616,7 +604,7 @@ var cards = [
 
 {
     'title': 'Unguarded Graveyard',
-    'text': '+1 :Treasure:.<br>At the start of your turn, if this card is in your stash: You may put a card from your stash at the bottom of your deck.',
+    'text': '+1 :Coin:.<br>At the start of your turn, if this card is in your stash: You may put a card from your stash at the bottom of your deck.',
     'img': 'structures/grave-robbers.png',
     'type': 'structure',
     'victory': '2',
@@ -628,7 +616,8 @@ var cards = [
 
 {
     'title': 'Bewitched Doll',
-    'text': '+1 :Treasure:. On target enemy’s next turn you control their first Action. You make all decisions arising from this Action, including any extra Action.',
+    'text': `+1 :Coin:. On target enemy’s next turn, control their first Action 
+    making all decisions of this enemy, including any extra Actions that may arise from it.`,
     // 'flavor': "Destiny is a fickle concept.",
     'img': 'attacks/voodoo-doll.png',
     'type': 'attack',
@@ -640,7 +629,7 @@ var cards = [
 
 {
     'title': 'Crooked Merchant',
-    'text': 'Pay 1 :Treasure:: Swap a card between your stash and target enemy’s stash as long as the cost difference is 1 or less.',
+    'text': 'Pay 1 :Coin:: Swap a card between your stash and target enemy’s stash as long as the cost difference is 1 or less.',
     'flavor': 'The lure of wealth leads to deceptive deals.',
     'img': 'attacks/merchant.png',
     'type': 'attack',
@@ -652,7 +641,7 @@ var cards = [
 
 {
     'title': 'Merfolk Mercenary',
-    'text': 'Pay 1 :Treasure:: Reveal the top 2 cards of the supply deck. Put one revealed card that cost 3 or less into your stash.',
+    'text': 'Pay 1 :Coin:: Reveal the top 2 cards of the supply deck. Put one revealed card that cost 3 or less into your stash.',
     'flavor': 'Is the bounty worth dealing with this abomination?', // Though she secures priceless treasures, i
     'img': 'adventures/merfolk-2.png',
     'type': 'adventure',
@@ -686,7 +675,7 @@ var cards = [
 
 {
     'title': 'Treasure Hunt',
-    'text': '+1 :Card:, +1 :Treasure:<br>+1 :Action:, +1 :Buy:.',
+    'text': '+1 :Card:, +1 :Coin:,<br>+1 :Action:, +1 :Buy:.',
     'flavor': 'An isle shrouded in mystery holds the key to immense wealth.',
     'img': 'adventures/hunt.png',
     'type': 'adventure',
