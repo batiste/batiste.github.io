b25cae572163575bc01b960a570c7e7fda5f7d20
Batiste Bieler <batiste@nop.ch>
2024-06-04 14:44:52 +0100

    DeckHand 2, starting

---
+++ deckhand2/cards.en.js
@@ -0,0 +1,24 @@
+
+
+var cards = [
+
+    {
+        "title": "Wild Parrot",
+        "coin": 2,
+        "cost": ['p', 'p', 'c'],
+        "img": "cards/parrot.png",
+        "type": "wild",
+        "build": ['p', 'p']
+    },
+    {
+        "title": "Military Power",
+        "coin": 3,
+        "cost": ['c', 'c', 'c', 'g'],
+        "img": "cards/military.png",
+        "type": "military",
+        "text": "",
+        "build": ['g', 'g']
+    }
+
+
+]
