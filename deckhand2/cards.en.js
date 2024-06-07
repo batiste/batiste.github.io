ff736f9ecad1c2cb61cabed777de2975ea29f90d
Batiste Bieler <batiste@nop.ch>
2024-06-07 13:41:25 +0100

    Add a few cards

--- deckhand2/cards.en.js
+++ deckhand2/cards.en.js
@@ -9,26 +9,36 @@ var cards = [
         "img": "cards/parrot.png",
         "type": "wild",
         "build": ['p'],
-        "play_text": `Draw a card, <img src="img/parrot.png" class="gun">`,
+        "play_text": `:parrot: and draw a card.`,
     },
     {
         "title": "Cannoneer's Guild",
         "coin": 3,
-        "cost": ['g', 'r', 'r', 'c', 'c', 'c'],
+        "cost": ['g', 'r', 'r', 'c', 'c'],
         "img": "cards/military.png",
         "type": "military",
-        "play_text": `<img src="img/gun.png" class="gun"> <img src="img/gun.png" class="gun">`,
-        "build_text": `<img src="img/hammer.png" class="hammer"> : <img src="img/gun.png" class="gun"> in
-            the cost of your cards are worth <img src="img/coin.png" class="coin">.`,
+        "play_text": `:gun: :gun:`,
+        "build_text": `:hammer: <b>:</b> :gun: <b>=</b> :coin:`,
         "build": ['g']
     },
+
     {
-        "title": "Plunder",
+        "title": "Walk the Plank",
         "coin": 1,
         "cost": ['c', 'c'],
+        "img": "cards/walk.png",
+        "type": "military",
+        "play_text": `:gun: and draw a card.`,
+        "build": ['g']
+    },
+
+    {
+        "title": "Plunder",
+        "coin": 2,
+        "cost": ['c', 'g'],
         "img": "cards/plunder.png",
         "type": "military",
-        "play_text": `Remove a card from you hand or discard from the game.`,
+        "play_text": `Draw 2 cards.`,
         "build": ['g']
     },
 
@@ -38,7 +48,7 @@ var cards = [
         "cost": ['p', 'p', 'c'],
         "img": "cards/sanctuary.png",
         "type": "wild",
-        "play_text": `<img src="img/coin.png" class="coin"> for each <img src="img/parrot.png" class="gun"> built.`,
+        "play_text": `:parrot: <b>=</b> :coin:.`,
         "build": ['p', 'p']
     },
 
@@ -48,7 +58,6 @@ var cards = [
         "cost": ['r', 'c'],
         "img": "cards/merchant.jpeg",
         "type": "merchant",
-        // "play_text": `<img src="img/coin.png" class="coin"> for each <img src="img/parrot.png" class="gun"> built.`,
         "bid_text": `If you win the bid, draw a card.`,
         "build": ['r']
     },
@@ -59,7 +68,7 @@ var cards = [
         "cost": ['c', 'c'],
         "img": "cards/merchant2.png",
         "type": "merchant",
-        "play_text": `<img src="img/coin.png" class="coin"> <img src="img/rum.png" class="rum">`,
+        "play_text": `:coin: :rum:`,
         // "bid_text": `If you win a bid with this card, draw a card.`,
         "build": ['r']
     },
@@ -70,7 +79,7 @@ var cards = [
         "cost": ['p', 'c'],
         "img": "cards/freed_monkeys.jpeg",
         "type": "wild",
-        "play_text": `<img src="img/parrot.png" class="hammer"> <img src="img/gun.png" class="coin"> <img src="img/rum.png" class="rum">`,
+        "play_text": `:gun: :rum:`,
         // "bid_text": `If you win a bid with this card, draw a card.`,
         "build": ['p', 'r']
     },
@@ -81,9 +90,61 @@ var cards = [
         "cost": ['g', 'p'],
         "img": "cards/warrior.jpeg",
         "type": "military",
-        // "play_text": `Draw 1 card.`,
-        "bid_text": `<img src="img/parrot.png" class="coin"> gain 1<img src="img/coin.png" class="coin"> `,
-        // "bid_text": `If you win a bid with this card, draw a card.`,
+        "bid_text": `:parrot: <b>=</b> :coin: `,
         "build": ['g']
+    },
+
+    {
+        "title": "Rich Pirate",
+        "coin": 1,
+        "cost": ['c', 'c', 'c'],
+        "img": "cards/rich.png",
+        "type": "merchant",
+        "play_text": `:coin::coin:`,
+        "build": ['c']
+    },
+
+    {
+        "title": "Animal Market",
+        "coin": 2,
+        "cost": ['p', 'c', 'r'],
+        "img": "cards/animal_market.png",
+        "type": "merchant",
+        "play_text": `:parrot::coin:`,
+        "build_text": `:coin: <b>=</b> :parrot:`,
+        "build": ['r']
+    },
+
+    {
+        "title": "Wild Winds",
+        "coin": 2,
+        "cost": ['p', 'g', 'r', 'c'],
+        "img": "cards/wind.png",
+        "type": "wild",
+        // "play_text": `:coin::coin:`,
+        "build": ['p'],
+        "build_text": `Draw an extra card at cleanup`
+    },
+
+    {
+        "title": "Loaded Cargo",
+        "coin": 1,
+        "cost": ['r', 'p'],
+        "img": "cards/loaded.png",
+        "type": "merchant",
+        "bid_text": `:rum: <b>=</b> :coin:`,
+        "build_text": `:rum: <b>=</b> :coin:`,
+        "build": ['r'],
+    },
+
+    {
+        "title": "Armed Orangutan",
+        "coin": 2,
+        "cost": ['r', 'p', 'g'],
+        "img": "cards/armed-orangutan.jpeg",
+        "type": "wild",
+        "bid_text": `:parrot::gun: <b>=</b> :coin:`,
+        "build_text": `:parrot::gun: <b>=</b> :coin::coin::coin:`,
+        "build": ['p'],
     }
 ]
