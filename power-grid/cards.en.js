5e19265a3155a7099cf4703bb14ee6d4fa666005
Batiste Bieler <batiste@nop.ch>
2024-08-31 15:01:19 +0100

    4 cards

--- power-grid/cards.en.js
+++ power-grid/cards.en.js
@@ -2,10 +2,31 @@
 
 var cards = [
     {
-        "title": "Hydroelectric Dam",
+        "title": "Water Grant",
         "discard": ['water'],
         "cost": ['dollar', 'dollar'],
-        "img": "cards/nuclear-2.png",
+        "img": "cards/water.png",
+        "production": ['water']
+    },
+    {
+        "title": "Cement Factory",
+        "discard": ['cement'],
+        "cost": ['dollar', 'dollar'],
+        "img": "cards/cement.png",
+        "production": ['cement']
+    },
+    {
+        "title": "Hydroelectric",
+        "discard": ['water'],
+        "cost": ['water', 'cement', 'dollar'],
+        "img": "cards/hydro.png",
         "production": ['energy', 'energy']
+    },
+    {
+        "title": "Nuclear",
+        "discard": ['energy'],
+        "cost": ['water', 'cement', 'dollar', 'dollar'],
+        "img": "cards/nuclear-2.png",
+        "production": ['energy', 'energy', 'energy']
     },
 ]
